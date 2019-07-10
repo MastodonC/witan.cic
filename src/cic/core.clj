e0da9c99d0dd1a4d9c13ef30ed4aec026d7914ca
Seb Bailey <sebastian@mastodonc.com>
2019-07-10 15:17:30 +0100

    Load data, remove unwanted rows

--- src/cic/core.clj
+++ src/cic/core.clj
@@ -1,7 +1,25 @@
 (ns cic.core
-  (:gen-class))
+  (:require [clojure.data.csv :as data-csv]
+            [clojure.java.io :as io]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn blank-row? [row]
+  (every? #(= "" %) row))
+
+(defn load-csv
+  "Loads csv file with each row as a vector.
+   Stored in map separating column-names from data"
+  ([filename]
+   (let [parsed-csv (with-open [in-file (io/reader filename)]
+                      (into [] (->> in-file
+                                    data-csv/read-csv
+                                    (remove (fn [row] (blank-row? row))))))
+         parsed-data (rest parsed-csv)
+         headers (map keyword (first parsed-csv))]
+     (map (fn [row] (reduce into {} (map (fn [k v] (assoc {} k v)) headers row))) parsed-data))))
+
+(defn remove-UASCs-and-V4s [data]
+  (remove #(or (= "V4" (:legal_status %)) (= true (:UASC %))) data))
+
+(defn -main [filename]
+  (let [data (load-csv filename)]
+    (remove-UASCs-and-V4s data)))
