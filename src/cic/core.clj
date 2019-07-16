0f4ffcf73c2271b20500b01f33f7e97cfdc83a15
Henry Garner <henry@henrygarner.com>
2019-07-16 10:22:49 +0100

    Parse field values

--- src/cic/core.clj
+++ src/cic/core.clj
@@ -1,24 +1,39 @@
 (ns cic.core
-  (:require [clojure.data.csv :as data-csv]
-            [clojure.java.io :as io]))
+  (:require [camel-snake-kebab.core :as csk]
+            [clojure.data.csv :as data-csv]
+            [clojure.java.io :as io]
+            [clojure.string :refer [blank?]]
+            [tick.alpha.api :as t]))
 
 (defn blank-row? [row]
-  (every? #(= "" %) row))
+  (every? blank? row))
+
+(defn format-row
+  [row]
+  (-> row
+      (update :report-date t/date)
+      (update :ceased #(when-not (blank? %) (t/date %)))
+      (update :report-year #(Long/parseLong %))
+      (update :placement keyword)
+      (update :care-status keyword)
+      (update :legal-status keyword)
+      (update :uasc (comp boolean #{"True"}))))
 
 (defn load-csv
   "Loads csv file with each row as a vector.
    Stored in map separating column-names from data"
   ([filename]
    (let [parsed-csv (with-open [in-file (io/reader filename)]
-                      (into [] (->> in-file
-                                    data-csv/read-csv
-                                    (remove (fn [row] (blank-row? row))))))
+                      (->> in-file
+                           data-csv/read-csv
+                           (remove blank-row?)
+                           (vec)))
          parsed-data (rest parsed-csv)
-         headers (map keyword (first parsed-csv))]
-     (map (fn [row] (reduce into {} (map (fn [k v] (assoc {} k v)) headers row))) parsed-data))))
+         headers (map csk/->kebab-case-keyword (first parsed-csv))]
+     (map (comp format-row (partial zipmap headers)) parsed-data))))
 
 (defn remove-UASCs-and-V4s [data]
-  (remove #(or (= "V4" (:legal_status %)) (= true (:UASC %))) data))
+  (remove (some-fn :uasc (comp #{"V4"} :legal_status)) data))
 
 (defn -main [filename]
   (let [data (load-csv filename)]
