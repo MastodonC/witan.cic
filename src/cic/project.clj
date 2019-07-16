8f236918828f34393babcf1fda68dd88ac3553b7
Henry Garner <henry@henrygarner.com>
2019-07-16 18:04:20 +0100

    Create simple base projection based on mean lifetime

--- src/cic/project.clj
+++ src/cic/project.clj
@@ -1,7 +1,10 @@
 (ns cic.project
   (:require [clojure.test.check.random :as r]
+            [clojure.string :as str]
+            [kixi.stats.core :as k]
             [kixi.stats.distribution :as d]
             [kixi.stats.protocols :as p]
+            [redux.core :as redux]
             [tick.alpha.api :as t]
             [tick.core :as tick]))
 
@@ -32,3 +35,87 @@
                  (assoc :birthday birthday)
                  (assoc :admission-age (quot (t/days (interval-duration birthday beginning)) 365)))))
          open-periods rngs)))
+
+(def lifetime
+  "FIXME: the distribution paramaters will depend on admission age, placement..."
+  (d/weibull {:shape 0.7131747 :scale 1020.9653067}))
+
+(defn lifetime-gt
+  "Sample a value > threshold from the distribution"
+  [threshold]
+  (loop [sample (int (d/draw lifetime))]
+    (if (> sample threshold)
+      sample
+      (recur (int (d/draw lifetime))))))
+
+(defn project-period-close
+  "Sample a possible duration in care which is greater than the existing duration"
+  [{:keys [duration beginning] :as open-period}]
+  (let [projected-duration (lifetime-gt duration)]
+    (-> (assoc open-period :duration projected-duration)
+        (assoc :end (tick/forward-number beginning projected-duration))
+        (assoc :open? false))))
+
+(defn day-seq
+  "Create a sequence of dates with a 7-day interval between two dates"
+  [beginning end]
+  (when (t/<= beginning end)
+    (lazy-seq
+     (cons beginning
+           (day-seq (tick/forward-number beginning 7) end)))))
+
+(defn daily-summary
+  "Takes inferred future periods and calculates the total CiC"
+  [periods beginning end]
+  (-> (reduce
+       (fn [[output candidates] date]
+         (let [candidates (filter (fn [{:keys [end]}]
+                                    (t/>= end date))
+                                  candidates)]
+           (vector (assoc output date (count candidates)) candidates)))
+       [{} periods]
+       (day-seq beginning end))
+      (first)))
+
+(defn project-1
+  [periods beginning end]
+  (-> (map project-period-close open-periods)
+      (daily-summary beginning end)))
+
+(defn vals-histogram
+  "Histogram reducing function for the the vals corresponding to `key`.
+  Returns a summary of the histogram"
+  [key]
+  (-> k/histogram
+      (redux/pre-step #(get % key))
+      (redux/post-complete d/summary)))
+
+(defn summarise
+  "Creates a histogram reducing function over each key of the maps returned by the runs.
+  Each histogram is summarised into a map with the date associated."
+  [runs]
+  (let [dates (->> runs first keys)
+        rf-spec (reduce (fn [rf-spec date]
+                          (assoc rf-spec date (vals-histogram date)))
+                        {}
+                        dates)
+        rf (redux/fuse rf-spec)]
+    (->> (transduce identity rf runs)
+         (map (fn [[k v]] (assoc v :date k))))))
+
+(defn projection
+  "Takes the open periods, creates n-runs projections and summarises them."
+  [open-periods beginning end n-runs]
+  (->> (repeatedly n-runs #(project-1 open-periods beginning end))
+       (summarise)))
+
+(defn format-projection
+  [projection]
+  (let [fields (juxt (comp str :date) :min :q1 :median :q3 :max)
+        field-sep "\t"
+        line-sep "\n"]
+    (->> (for [future-estimate projection]
+           (str/join field-sep (fields future-estimate)))
+         (str/join line-sep))))
+
+;; (format-projection (project (map project-period-close (prepare-ages open-periods 42)) (t/date "2018-03-31") (t/date "2025-03-31") 1000))
