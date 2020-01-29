f0f3ab0bcc1e0bf04bdfad0a61f3302f15a59ae0
Bruce Durling <bruce@mastodonc.com>
2020-01-29 18:13:59 +0000

    wip

--- src/cic/repl.clj
+++ src/cic/repl.clj
@@ -15,6 +15,8 @@
             [net.cgrand.xforms.rfs :as xrf]
             [kixi.stats.core :as k]))
 
+(set! *warn-on-reflection* true)
+
 (defn load-model-inputs
   "A useful REPL function to load the data files and convert them to  model inputs"
   ([{:keys [episodes-csv placement-costs-csv duration-lower-csv duration-median-csv duration-upper-csv]}]
@@ -240,18 +242,27 @@
            project-from (time/max-date (map :beginning periods))
            project-to (time/years-after project-from 3)
            project-dates (time/day-seq project-from project-to 7)
-           n-runs 10
+           n-runs 1000
            learn-from (time/years-before project-from 4)
            projection-seed {:seed (filter :open? periods)
                             :date project-from}
            model-seed {:seed periods
                        :duration-model duration-model
                        :joiner-range [learn-from project-from]}
-           n-projections (into [] (projection/project-n projection-seed model-seed project-dates seed n-runs))
+           ;; n-projections (into [] (projection/project-n projection-seed model-seed project-dates seed n-runs))
 
            ;; reducing taps
            single-projection (a/into [] (a/take 1 (a/tap input-mult (a/chan 32))))
            all-projections (a/into [] (a/tap input-mult (a/chan 512)))
+
+           ;; TODO: use placement types and expected ages to
+           ;; pre-populate base maps below so that we can just do
+           ;; (k/histogram hist int) rather than having to merge the
+           ;; histograms. This would also mean that we don't have to
+           ;; turn all the maps of counts into maps of histograms and
+           ;; thus create far fewer histograms.
+
+           ;; placements
            placement-summary-mult
            (a/mult
             (a/tap input-mult (a/chan 512 (map #(summary/placements-summary % project-dates)))))
@@ -269,16 +280,37 @@
                                                                     histogram-map))))
                                      {}
                                      (a/tap placement-summary-mult (a/chan 512)))
+
+           ;; ages
+           ages-summary-mult
+           (a/mult
+            (a/tap input-mult (a/chan 512 (map #(summary/ages-summary % project-dates)))))
+           ages-summary-mapped-results (a/into [] (a/tap ages-summary-mult (a/chan 32)))
+           ages-weekly-summary (a/transduce
+                                (comp
+                                 (mapcat identity)
+                                 (map (fn [[k v]] [k (map-of-counts->map-of-histograms v)])))
+                                (fn
+                                  ([] {})
+                                  ([acc] (into {} (map (fn [[k v]] [k (summarize-map-of-histograms v)])) acc))
+                                  ([acc [time histogram-map]]
+                                   (assoc acc time (merge-with (fnil merge-histograms {})
+                                                               (get acc time)
+                                                               histogram-map))))
+                                {}
+                                (a/tap ages-summary-mult (a/chan 512)))
            ]
 
        ;; put the projections on the channel
-       (a/pipe (a/to-chan n-projections) input-chan)
-       ;; (a/pipe (a/to-chan (projection/project-n projection-seed model-seed project-dates seed n-runs)) input-chan)
+       ;; (a/pipe (a/to-chan n-projections) input-chan)
+       (a/pipe (a/to-chan (projection/project-n projection-seed model-seed project-dates seed n-runs)) input-chan)
 
        {:placement-weekly-summary (a/<!! placement-weekly-summary)
         :single-projection (a/<!! single-projection)
         :all-projections (a/<!! all-projections)
         :placement-summary-mapped-results (a/<!! placement-summary-mapped-results)
+        :ages-summary-mapped-results (a/<!! ages-summary-mapped-results)
+        :ages-weekly-summary (a/<!! ages-weekly-summary)
         })))
 
 
