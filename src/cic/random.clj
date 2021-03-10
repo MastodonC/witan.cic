893f261e4d2423008a57d7f8becd0a043e189881
Henry Garner <henry@henrygarner.com>
2021-03-10 19:17:12 +0000

    Don't close assign end dates in the past

--- src/cic/random.clj
+++ src/cic/random.clj
@@ -52,27 +52,31 @@
   [periods projection-model age-out-model age-out-projection-model seed]
   (println "Closing open periods...")
   (->> (for [{:keys [period-id beginning open? admission-age birthday] :as period} periods
-             :let [age-out? (age-out-model admission-age seed)]]
+             :let [age-out? (age-out-model admission-age seed)
+                   current-duration (:duration period)]]
          (if open?
-           (if-let [{:keys [episodes-edn duration]} (if age-out?
-                                                      (or (age-out-projection-model period-id)
-                                                          (projection-model period-id))
-                                                      (projection-model period-id))]
-             (let [duration (if (or age-out? (>= (time/year-interval birthday (time/days-after beginning duration)) 17))
-                              ;; Either we wanted to age out, or they did by virtue of staying beyond 17th birthday
-                              (periods/max-duration period)
-                              (min duration (periods/max-duration period)))]
-               (assoc period
-                      :episodes (read-string episodes-edn)
-                      :duration duration
-                      :end (time/days-after beginning duration)
-                      :provenance "P"))
-             ;; If we can't close a case, it's almost certainly
-             ;; because they are an aged-out case. Set max duration
-             (let [duration (periods/max-duration period)]
-               (assoc period
-                      :duration duration
-                      :end (time/days-after beginning duration)
-                      :provenance "P")))
+           (loop [iter 1]
+             (if-let [{:keys [episodes-edn duration]} (if age-out?
+                                                        (or (age-out-projection-model period-id)
+                                                            (projection-model period-id))
+                                                        (projection-model period-id))]
+               (if (and (< duration current-duration) (< iter 1000))
+                 (recur (inc iter))
+                 (let [duration (if (or age-out? (>= (time/year-interval birthday (time/days-after beginning duration)) 17))
+                                  ;; Either we wanted to age out, or they did by virtue of staying beyond 17th birthday
+                                  (periods/max-duration period)
+                                  (min duration (periods/max-duration period)))]
+                   (assoc period
+                          :episodes (read-string episodes-edn)
+                          :duration duration
+                          :end (time/days-after beginning duration)
+                          :provenance "P")))
+               ;; If we can't close a case, it's almost certainly
+               ;; because they are an aged-out case. Set max duration
+               (let [duration (periods/max-duration period)]
+                 (assoc period
+                        :duration duration
+                        :end (time/days-after beginning duration)
+                        :provenance "P"))))
            (assoc period :provenance "H")))
        #_(keep identity)))
