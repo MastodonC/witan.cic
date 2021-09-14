08455c692872c81eb150ce934758f536fb77b9a1
seb231@me.com <seb231@me.com>
2021-09-14 15:50:10 +0100

    Check R script exists before execution

--- src/cic/rscript.clj
+++ src/cic/rscript.clj
@@ -5,10 +5,13 @@
             [clojure.java.shell :as shell]
             [clojure.set :as cs]
             [clojure.string :as str]
+            [clojure.java.io :as io]
             [taoensso.timbre :as log]))
 
 (defn exec
   [script-path & args]
+  (when-not (.exists (io/file script-path))
+    (throw (ex-info "File does not exist" {:file-path script-path})))
   (log/info (format "Executing %s %s" script-path (str/join " " args)))
   (let [return-val (apply shell/sh "Rscript" "--vanilla" script-path args)]
     ;; rscript is quite chatty, so only pass on err text if exit was abnormal
