de726375dd0d7428eb6c446c0bac5625b1c78e72
seb231@me.com <seb231@me.com>
2021-09-14 11:33:11 +0100

    Better logging of problems running R

--- src/cic/rscript.clj
+++ src/cic/rscript.clj
@@ -4,17 +4,18 @@
             [clj-time.format :as f]
             [clojure.java.shell :as shell]
             [clojure.set :as cs]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [taoensso.timbre :as log]))
 
 (defn exec
   [script-path & args]
-  (println (format "Executing %s %s" script-path (str/join " " args)))
+  (log/info (format "Executing %s %s" script-path (str/join " " args)))
   (let [return-val (apply shell/sh "Rscript" "--vanilla" script-path args)]
     ;; rscript is quite chatty, so only pass on err text if exit was abnormal
-    (when (not= 0 (:exit return-val))
-      (apply println script-path args)
-      (println (:err return-val)))
-    (println (format "Executed %s" script-path))))
+    (if (not= 0 (:exit return-val))
+      (do (log/error script-path args)
+          (log/error return-val))
+      (log/info (format "Executed %s" script-path)))))
 
 (def date-format
   (f/formatter :date-hour-minute-second))
