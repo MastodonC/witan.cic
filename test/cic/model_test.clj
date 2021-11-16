022ac83a7e2a7d130daecf97a8f2efef95923d5d
Henry Garner <henry@henrygarner.com>
2021-11-16 16:44:10 +0000

    Add markov-period test

--- test/cic/model_test.clj
+++ test/cic/model_test.clj
@@ -4,6 +4,11 @@
             [cic.time :as time]
             [clojure.test :refer :all]))
 
+(defn rejection-model
+  [rejection-proportions]
+  (fn [admission-age duration-days provenance]
+    true))
+
 (deftest joiners-model-test
   (let [coefs {"(Intercept)" 0
                "admission_age0" 0.5
@@ -16,3 +21,27 @@
     (is (= 15.233248281888217
            (model 0 project-from project-from seed)))))
 
+(deftest markov-period-test
+  (let [period {:episodes [{:placement :Q1 :offset 0}]
+                :birthday (time/make-date 2010 1 1)
+                :beginning (time/make-date 2020 2 1)
+                :duration 2000
+                :period-id "abc"
+                :provenance "S"
+                :seed (random/seed 42)}
+        offset-segments {[2000 :Q1] [{:terminal? true
+                                      :episodes [{:placement :Q1 :offset 0}]
+                                      :duration 6500
+                                      :to-placement :Q1
+                                      :aged-out? true}]}
+        rejection-model (rejection-model {})]
+    (is (= {:beginning (time/make-date 2020 2 1)	  
+	    :birthday (time/make-date 2010 1 1)
+	    :duration 2890
+	    :episodes [{:placement :Q1, :offset 0}]
+	    :period-id "abc"
+	    :open? false
+	    :iterations 0
+	    :provenance "S"
+	    :end (time/make-date 2027 12 31)}
+           (dissoc (sut/markov-period period offset-segments rejection-model) :seed)))))
